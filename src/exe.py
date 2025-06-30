c64fd47372bec308fc2bb9d5c18ac5c6b63f89b0
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-30 20:00:36 +0200

    refactor: Extract help text to external file

--- src/exe.py
+++ src/exe.py
@@ -1,11 +1,9 @@
 import telepot
 import pandas as pd
+import os
 
 token = token_number
 bot = telepot.Bot(token)
-
-
-
 
 #------------------------printer----------------------------
 
@@ -28,23 +26,10 @@ def poster(name, id):
         bot.sendMessage(id, "다른 검색어를 넣어 보실래요?")
 
 # -------------------------도움말 Markdown ---------------------
-text = '''
-안녕하세요? 👋 [@Fantasticubot](https://t.me/Fantasticubot)이라고 해요. 
-제가 할 수 있는 일은요... ¯\\\_(ツ)\_/¯  
-  
-📌\[상영표] 
-*상영표*라고 입력해보세요.
-오늘 상영하는 영화와 시간을 알려줘요.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-📌\[연락처]
-*주소* 혹은 *연락처*라고 입력해보세요.
-판타스틱큐브의 주소와 연락처를 알려줘요.
-
-📌\[포스터]
-*영화 제목*을 입력해보세요.
-판타스틱큐브에서 상영 중인 영화의 포스터들을 보여줘요. 영화 제목의 일부만 입력해도 찾아줘요. 🤘
-
-'''
+with open(os.path.join(BASE_DIR, "texts", "help.md"), "r", encoding="utf-8") as f:
+    text = f.read()
 
 #---------------------------텔레그램 메시지 봇----------------------------
 def handle(msg):
